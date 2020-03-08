32157010047b3c171f24e2557b7574b28509524c
Valeria Dhamary  Manriquez Aravena <valeria.manriquez90@gmail.com>
2020-03-08 01:17:48 -0300

    Ejercicio_1_while_mientras
    Con Verificador de datos int

--- 04_Estructuras_Repetitivas/src/Guia_Estructutras_Repetitivas_04/Ejercicio_1_while_mientras.java
+++ 04_Estructuras_Repetitivas/src/Guia_Estructutras_Repetitivas_04/Ejercicio_1_while_mientras.java
@@ -9,8 +9,6 @@ import java.util.Scanner;
 public class Ejercicio_1_while_mientras {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 		Scanner sc = new Scanner(System.in);
 
 		String numero;
